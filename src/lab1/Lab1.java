609596bbfe9692b721bfc11fa55be13e1f49cedc
Кирилл Никифоров <kirill2kxaos@mail.ru>
2022-09-20 00:23:13 +0300

    Finish 1 lab

---
+++ src/lab1/Lab1.java
@@ -0,0 +1,36 @@
+
+package lab1;
+
+/**
+ *
+ * @author Nikifor
+ */
+public class Lab1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Integer even = 0;
+       Integer uneven = 0;
+ for (String x : args) {
+ 
+ if (Integer.parseInt(x) % 2 == 0 && Integer.parseInt(x) >0) {// четные положительные
+ even = even + 1;
+ } else if (Integer.parseInt(x) % 2 != 0 && Integer.parseInt(x) >0){//«нечётных и положительных
+ uneven = uneven + 1;
+ }
+ }
+ 
+ if (even > uneven){
+ System.out.println("even");//вывод "чётного" 
+ }
+ else if (even < uneven){
+ System.out.println("uneven");//вывод "нечётного"
+ }
+ else{
+ System.out.println("="); 
+ }
+   }
+    
+}
