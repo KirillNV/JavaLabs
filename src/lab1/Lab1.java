805265e80a271bec91fce476f5d6d3f9205ec008
Кирилл Никифоров <kirill2kxaos@mail.ru>
2022-09-20 00:27:38 +0300

    Finish 1 lab!

--- src/lab1/Lab1.java
+++ src/lab1/Lab1.java
@@ -7,9 +7,7 @@ package lab1;
  */
 public class Lab1 {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
        Integer even = 0;
        Integer uneven = 0;
